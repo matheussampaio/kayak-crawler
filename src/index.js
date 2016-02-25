121ea30529d7f5d1a2e897fdff13258d6105f823
Matheus Sampaio <matheus1401@gmail.com>
2016-02-24 20:23:36 -0600

    search flight on kayak

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
 require(`babel-polyfill`);
+require(`date-utils`);
 
 import co from 'co';
 import Nightmare from 'nightmare';
@@ -13,12 +14,8 @@ function main() {
     fromAirport: `CHI`,
     toAirport: `JPA`,
     departDate: `2016-05-04`,
-    returnDate: `2016-05-18`
+    returnDate: `2016-05-04`
   });
-}
-
-function getSearchURL({ fromAirport, toAirport, departDate, returnDate }) {
-  return `https://www.kayak.com/flights/${fromAirport},nearby-${toAirport},nearby/${departDate}-flexible/${returnDate}-flexible`;
 }
 
 function search({ fromAirport, toAirport, departDate, returnDate }) {
@@ -27,22 +24,47 @@ function search({ fromAirport, toAirport, departDate, returnDate }) {
     dock: true
   });
 
-  const searchURL = getSearchURL({ fromAirport, toAirport, departDate, returnDate });
+  let lowerPriceValue = Number.MAX_VALUE;
+  let lowerPriceDate = ``;
 
   return co.wrap(function* submitGenerator() {
-    // Load Page
-    yield browser.goto(searchURL);
+    console.log(`Starting search...`);
 
-    // // Check if must close Dialog
-    // const popupVisible = yield browser.visible(`r9-dialog-closeButton r9-icon-x  tighter`);
-    //
-    // if (popupVisible) {
-    //   console.log(`closing popup`);
-    //   yield browser.click(`r9-dialog-closeButton r9-icon-x  tighter`);
-    // } else {
-    //   console.log(`popup didn't show.`);
-    // }
+    for (let i = 0; i < 40; i++) {
+      returnDate = getNextDate(returnDate);
+      const searchURL = getSearchURL({ fromAirport, toAirport, departDate, returnDate });
 
+      // Load Page
+      yield browser.goto(searchURL);
+
+      // yield closeDialog(browser);
+
+      yield waitSearchFinish(browser);
+
+      // Get Prices
+      const prices = yield getPrices(browser);
+
+      const temp = Math.min.apply(Math, prices);
+
+      console.log(`${returnDate}: $${temp}`);
+
+      if (temp <= lowerPriceValue) {
+        lowerPriceValue = temp;
+        lowerPriceDate = returnDate;
+      }
+    }
+
+    console.log(`\n\nThe best price is: ${lowerPriceDate} -> $${lowerPriceValue}`);
+
+    return yield browser.end();
+  })()
+  .catch(error => {
+    console.error(error.stack ? error.stack : error);
+  });
+}
+
+function waitSearchFinish(browser) {
+  return co(function* () {
     // Wait the search finish
     let progressDiv = yield browser.visible(`#progressDiv`);
 
@@ -50,28 +72,49 @@ function search({ fromAirport, toAirport, departDate, returnDate }) {
       yield browser.wait(1000);
       progressDiv = yield browser.visible(`#progressDiv`);
     }
-    console.log(`Progress Finished`);
+  });
+}
 
-    // Get Prices
-    yield browser.inject(`js`, `./dist/jquery.min.js`);
+function getPrices(browser) {
+  return co(function* () {
+    yield browser.inject(`js`, `./node_modules/jquery/dist/jquery.min.js`);
 
     const prices = yield browser.evaluate(() => {
       return $(`#flexmatrixcontent .data > a`)
         .text()
         .trim()
         .split(` `)
-        .map(elem => {
-          return parseInt(elem.replace(`\n`, ``).replace(`$`, ``), 10);
-        });
+        .map(elem => parseInt(elem.replace(`\n`, ``).replace(`$`, ``), 10));
     });
 
-    console.log(prices);
-
-    yield browser.wait(5000);
-
-    // return yield browser.end();
-  })()
-  .catch(error => {
-    console.error(error.stack ? error.stack : error);
+    return yield prices;
   });
+}
+
+//
+// function closeDialog(browser) {
+//   return co(function* () {
+//     // Check if must close Dialog
+//     const popupVisible = yield browser.visible(`r9-dialog-closeButton r9-icon-x  tighter`);
+//
+//     if (popupVisible) {
+//       console.log(`closing popup`);
+//       yield browser.click(`r9-dialog-closeButton r9-icon-x  tighter`);
+//     } else {
+//       console.log(`popup didn't show.`);
+//     }
+//   });
+// }
+
+function getSearchURL({ fromAirport, toAirport, departDate, returnDate }) {
+  return [
+    `https://www.kayak.com/flights/`,
+    `${fromAirport},nearby-${toAirport},nearby/`,
+    `${departDate}-flexible/`,
+    `${returnDate}-flexible`
+  ].join(``);
+}
+
+function getNextDate(str) {
+  return new Date(str).add({ days: 8 }).toFormat(`YYYY-MM-DD`);
 }
